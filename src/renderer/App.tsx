bd9cb97ccb9cd28e1964ec1a81e9880dc79cf69b
kubko <hladikjakubis@gmail.com>
2024-09-11 08:00:59 +0200

    sssssss

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,40 +1,94 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import { DataConnection, Peer } from 'peerjs';
 import './App.css';
-
+import { useEffect, useRef, useState } from 'react';
+const upObj = {
+  direction: 0,
+  velocity: 100,
+};
+const downObj = {
+  direction: 180,
+  velocity: 100,
+};
+const leftObj = {
+  direction: 270,
+  velocity: 100,
+};
+const rightObj = {
+  direction: 90,
+  velocity: 100,
+};
 function Hello() {
+  const [peerId, setPeerId] = useState('');
+  const peerRef = useRef(new Peer());
+  const connectionRef = useRef<DataConnection | null>(null);
+  const localStreamRef = useRef<MediaStream | null>(null);
+  const remoteStreamRef = useRef<MediaStream | null>(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  function connect() {
+    connectionRef.current = peerRef.current.connect(peerId);
+  }
+  function call() {
+    if (!localStreamRef.current) return;
+    const call = peerRef.current.call(peerId, localStreamRef.current);
+    call.on('stream', (remoteStream) => {
+      if (!videoRef.current) return;
+      videoRef.current.srcObject = remoteStream;
+    });
+  }
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: true,
+      })
+      .then((stream) => {
+        localStreamRef.current = stream;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    peerRef.current.on('connection', (conn) => {
+      connectionRef.current = conn;
+      conn.on('data', (data) => {
+        console.log(data);
+      });
+    });
+    window.onkeydown = (e) => {
+      if (!connectionRef.current) return;
+      switch (e.key) {
+        case 'ArrowUp':
+          connectionRef.current.send(JSON.stringify(upObj));
+          break;
+        case 'ArrowDown':
+          connectionRef.current.send(JSON.stringify(downObj));
+          break;
+        case 'ArrowLeft':
+          connectionRef.current.send(JSON.stringify(leftObj));
+          break;
+        case 'ArrowRight':
+          connectionRef.current.send(JSON.stringify(rightObj));
+          break;
+        default:
+          break;
+      }
+    };
+  }, []);
   return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+    <div className="w-screen h-screen">
+      <video className="w-screen h-screen" ref={videoRef} />
+      <input
+        className="fixed top-0"
+        type="text"
+        onChange={(e) => setPeerId(e.target.value as string)}
+      />
+      <button className=" fixed top-0 left-64" onClick={connect}>
+        connect
+      </button>
+      <button className=" fixed top-0 left-96" onClick={call}>
+        call
+      </button>
     </div>
   );
 }
